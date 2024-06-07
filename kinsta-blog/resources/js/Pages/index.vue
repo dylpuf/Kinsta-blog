96b58e29d6c22ca787495655595dd5781c59e114
DH <Dylanhupsel@gmail.com>
2024-06-07 11:38:14 +0200

    Opdracht 3

---
+++ kinsta-blog/resources/js/Pages/index.vue
@@ -0,0 +1,16 @@
+<script setup> 
+    import KinstaLayout from "../Layouts/KinstaLayout.vue"; 
+</script> 
+<template> 
+<KinstaLayout> 
+<section> 
+<h2>Read our latest articles</h2> 
+<article> 
+<div> <img src="/images/kinsta-logo.png" alt="Article thumbnail" /> </div> 
+<h3>Title for the blog</h3> 
+<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rem itaque error vel perferendis aliquam numquam dignissimos, expedita perspiciatis consectetur! </p> 
+<a href="#">Read more</a> 
+</article> 
+</section> 
+</KinstaLayout> 
+</template>
